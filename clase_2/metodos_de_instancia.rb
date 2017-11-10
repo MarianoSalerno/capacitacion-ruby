3f144e440eb07cb41e50e0ba0c5aee4a5a2e9c44
Mariano Salerno <mariano.salerno@avantrip.com>
2017-11-10 16:51:17 -0300

    agregadas variables de clase

--- clase_2/metodos_de_instancia.rb
+++ clase_2/metodos_de_instancia.rb
@@ -8,11 +8,16 @@ class Persona
     "#{nombre} #{segundo_nombre} #{apellido}"
   end
 
-  def es_mufa? nombre_mufa
-    @nombre.include? nombre_mufa
+  def nombre_completo_named(nombre:, segundo_nombre:"Roque", apellido:)
+    "#{nombre} #{segundo_nombre} #{apellido}"
+  end
+
+  def es_mufa? nombre_mufa="Carlos"
+    @nombre.include? nombre_mufa.capitalize
   end
 
 end
 
-puts Persona.new.nombre_completo_splat("Mariano", "Roque", "Salerno")
 puts Persona.new.nombre_completo(*["Mariano", "Roque", "Salerno"])
+puts Persona.new.nombre_completo_splat("Mariano", "Roque", "Salerno")
+puts Persona.new.nombre_completo_named(nombre: "Mariano", apellido: "Salerno")
---
+++ clase_2/variables_de_clase.rb
@@ -0,0 +1,19 @@
+class Animal
+  @@peso_default = 5
+
+  def self.peso_default
+    @@peso_default
+  end
+end
+
+class Golondrina < Animal
+  @@peso_default = 10
+end
+
+class Colibri < Animal
+  @@peso_default = 1
+end
+
+puts Animal.peso_default
+puts Golondrina.peso_default
+puts Golondrina.peso_default
